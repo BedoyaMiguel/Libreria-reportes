6e165299e622457d60e767febd4057588c97131e
BedoyaMiguel <miguel.bedoya02@uceva.edu.co>
2026-06-05 16:34:38 -0500

    Mi libreria reportes

---
+++ nano-reportes.h
@@ -0,0 +1,24 @@
+#ifndef REPORTES_H
+#define REPORTES_H
+
+double calcular_costo_total(double directos, double indirectos);
+double calcular_costo_mensual(double total, int duracion_meses);
+double calcular_costo_por_persona(double mensual, int tamano_equipo);
+
+#endif
+-----------------------------------------------------------------------------------------------------------------------------
+#include "reportes.h"
+
+double calcular_costo_total(double directos, double indirectos) {
+    return directos + indirectos;
+}
+
+double calcular_costo_mensual(double total, int duracion_meses) {
+    if (duracion_meses <= 0) return 0;
+    return total / duracion_meses;
+}
+
+double calcular_costo_por_persona(double mensual, int tamano_equipo) {
+    if (tamano_equipo <= 0) return 0;
+    return mensual / tamano_equipo;
+}
